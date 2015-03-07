1805a8e6dc5b440dacc13382e2a1bae1a1599cd1
“romeocodemagnusgit config — gobal user.email “your_email@example.com <admin@nextsprout.com>
2015-03-07 23:45:37 +0800

    add plot2.R and plot2.png

---
+++ plot2.R
@@ -0,0 +1,16 @@
+## Read the dataset
+d <- read.table("household_power_consumption.txt", sep=";", header=T, colClasses=c("character", "character", rep("numeric", 7)), na.strings="?")
+## Convert the date column to Date class
+d$Date <- as.Date(d$Date, format="%d/%m/%Y")
+## Subset the data to filter using the desired dates:
+d <- d[d$Date >= as.Date("2007-02-01") & d$Date<=as.Date("2007-02-02"),]
+## Convert Date and Time columns to POSIXlt
+d$Date <- as.POSIXlt(paste(as.Date(d$Date, format="%d/%m/%Y"), d$Time, sep=" "))
+## Start a PNG device
+png("plot2.png", width=480, height=480)
+##Control the margins
+par(mar=c(5.0, 5.0, 1.2, 0.8))
+##Plot GAP vs. time
+plot(d$Date, d$Global_active_power, type="l",
+     xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
